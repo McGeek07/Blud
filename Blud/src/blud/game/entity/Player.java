2c1bb324bbb3b097d0b63071613242d7b7b56d85
Ghost <Ghost@192.168.1.188>
2019-08-02 10:46:15 -0600

    Player Class Made

--- Blud/src/blud/game/entity/Player.java
+++ Blud/src/blud/game/entity/Player.java
@@ -1,5 +1,17 @@
 package blud.game.entity;
 
-public class Player {
+public class Player extends Entity{
+
+	@Override
+	public void onRender(RenderContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpdate(UpdateContext context) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
