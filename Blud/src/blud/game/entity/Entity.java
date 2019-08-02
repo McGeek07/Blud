d1319c6dd5ac478abd264bb41b74a57ae46352de
Ghost <Ghost@192.168.1.188>
2019-08-02 11:13:46 -0600

    Working on battle functions

--- Blud/src/blud/game/entity/Entity.java
+++ Blud/src/blud/game/entity/Entity.java
@@ -2,8 +2,16 @@ package blud.game.entity;
 
 import blud.game.sprite.Sprite;
 import blud.geom.Vector;
+import blud.geom.Vector2f;
 
 public abstract class Entity extends blud.game.level.Object {
+	
+	int currentHealth;
+	int maxHealth;
+	int visibility;
+	boolean needLight;
+	boolean visibleThroughWalls;
+	Vector2f orientation;
 	
 	public Entity(Sprite... sprites) {
 		super(sprites);
@@ -16,4 +24,17 @@ public abstract class Entity extends blud.game.level.Object {
 	public Entity(float i, float j, Sprite... sprites) {
 		super(i, j, sprites);
 	}		
+	
+	public void move(Entity character) {
+		setLocal(Vector.add(local, blud.game.Game.EAST));
+	}
+	
+	public void fight(Entity attacker, Entity defender ) {
+		
+	}
+	
+	public void takeDamage(Entity character, int damageAmount) {
+		character.currentHealth -= damageAmount;
+	}
+	
 }
