889c8d491a2cdeef0ced7d44708dbd56eafd39cc
smcge <smcge@192.168.0.2>
2019-08-07 12:02:12 -0600

    Changes to camera in Player

--- Blud/src/blud/game/level/unit/units/Player.java
+++ Blud/src/blud/game/level/unit/units/Player.java
@@ -6,6 +6,7 @@ import blud.game.level.unit.Unit;
 import blud.game.sprite.Sprite;
 import blud.game.sprite.sprites.Sprites;
 import blud.geom.Vector;
+import blud.geom.Vector2f;
 
 public class Player extends Unit {
 	protected final Sprite
@@ -19,7 +20,7 @@ public class Player extends Unit {
 				);
 		this.playerVisionLevel = 1f;
 		this.playerVisionRange = 8f;
-		this.moveFrames   = 4;
+		this.moveFrames   = 8;
 		this.attackFrames = 8;
 		this.defendFrames = 12;
 		
@@ -46,28 +47,23 @@ public class Player extends Unit {
 	}
 
 	@Override
-	public void onUpdate(UpdateContext context) {		
+	public void onUpdate(UpdateContext context) {	
+		Vector2f camera = Game.pixelToLocal(node.level.camera);
 		float
-			dx = (pixel.X() - node.level.camera.X()) * .5f,
-			dy = (pixel.Y() - node.level.camera.Y()) * .5f;		
-		Vector.add(node.level.camera, dx, dy);
+			dx = (node.local.X() - camera.X()) * .5f,
+			dy = (node.local.Y() - camera.Y()) * .5f;		
+		Vector.add(node.level.camera, Game.localToPixel(dx, dy));
 		
 		int
 			w = Input.isKeyDn(Input.KEY_W) ? 1 : 0,
 			a = Input.isKeyDn(Input.KEY_A) ? 2 : 0,
 			s = Input.isKeyDn(Input.KEY_S) ? 4 : 0,
 			d = Input.isKeyDn(Input.KEY_D) ? 8 : 0;
-		boolean
-			isFacingUp = facing > 1,
-			isFacingDn = facing < 2,
-			isFacingL = (facing & 1) != 0,
-			isFacingR = (facing & 1) == 0;
 		switch(w | a | s | d) {
 			//case 0:
 			//case 5:
 			//case 10:
 			//case 15:			
-		
 			case  1: //w
 			case  9: //w + d
 			case 11: //w + a + d
